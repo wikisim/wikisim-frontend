81c01ca22368424894a1c521f26dc770cbd6e285
Alexander James Phillips <ajp.global@gmail.com>
2025-07-07 11:57:28 +0100

    Maybe improve how ts-jest is run

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 // Adapted from https://github.com/mtiller/ts-jest-sample/blob/master/jest.config.js
 export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.tsx?$": ["ts-jest", {
+            tsconfig: "tsconfig.test.json"
+        }],
     },
     testRegex: "((\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     testPathIgnorePatterns: ["/lib/", "/node_modules/"],
