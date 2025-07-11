63300e6e9c5bc26020c804852bf490e832ccaec2
Alexander James Phillips <ajp.global@gmail.com>
2025-07-12 00:10:03 +0100

    Swap from jest to mocha

--- src/text_editor/is_valid_URL.test.ts
+++ src/text_editor/is_valid_URL.test.ts
@@ -1,29 +1,31 @@
+import { expect } from "chai"
+
 import { is_valid_URL } from "./is_valid_URL"
 
 
 describe("is_valid_URL", () => {
     it("should return true for valid URLs", () => {
-        expect(is_valid_URL("https://example.com")).toBe(true)
-        expect(is_valid_URL("http://example.com")).toBe(true)
-        expect(is_valid_URL("www.example.com")).toBe(true)
-        expect(is_valid_URL("example.com")).toBe(true)
-        expect(is_valid_URL("a.com")).toBe(true)
-        expect(is_valid_URL("https://subdomain.example.com/path?query=123#fragment")).toBe(true)
-        expect(is_valid_URL("subdomain.example.com/path?query=123#fragment")).toBe(true)
+        expect(is_valid_URL("https://example.com")).equals(true)
+        expect(is_valid_URL("http://example.com")).equals(true)
+        expect(is_valid_URL("www.example.com")).equals(true)
+        expect(is_valid_URL("example.com")).equals(true)
+        expect(is_valid_URL("a.com")).equals(true)
+        expect(is_valid_URL("https://subdomain.example.com/path?query=123#fragment")).equals(true)
+        expect(is_valid_URL("subdomain.example.com/path?query=123#fragment")).equals(true)
         // handles trailing whitespace
-        expect(is_valid_URL("example.com  ")).toBe(true)
+        expect(is_valid_URL("example.com  ")).equals(true)
         // Seems to be handled as valid by URL constructor and browsers
-        expect(is_valid_URL("http:/invalid.com")).toBe(true)
+        expect(is_valid_URL("http:/invalid.com")).equals(true)
     })
 
     it("should return false for invalid URLs", () => {
-        expect(is_valid_URL("invalid-url")).toBe(false)
+        expect(is_valid_URL("invalid-url")).equals(false)
         // rejects prepended whitespace
-        expect(is_valid_URL("  example.com")).toBe(false)
-        expect(is_valid_URL("  example.com  ")).toBe(false)
+        expect(is_valid_URL("  example.com")).equals(false)
+        expect(is_valid_URL("  example.com  ")).equals(false)
     })
 
     it("should handle empty strings", () => {
-        expect(is_valid_URL("")).toBe(false)
+        expect(is_valid_URL("")).equals(false)
     })
 })
