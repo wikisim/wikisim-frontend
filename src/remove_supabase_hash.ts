e6e6c62947ea25da69f4060ef9f021c5a8a487c1
Alexander James Phillips <ajp.global@gmail.com>
2025-07-20 17:12:15 +0100

    Fix removing hash from URL

--- src/remove_supabase_hash.ts
+++ src/remove_supabase_hash.ts
@@ -2,7 +2,7 @@
 
 // When supabase auth is used, it will redirect to the page with a hash.
 // This code removes the hash from the URL.
-if (window.location.hash === "#")
+if (window.location.toString().endsWith("#"))
 {
     history.replaceState(null, "", window.location.pathname + window.location.search)
 }
