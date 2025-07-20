263460c1067e78faacceed81569723e849288dc3
Alexander James Phillips <ajp.global@gmail.com>
2025-07-20 16:04:21 +0100

    DataComponent card links to view page

--- src/ui_components/DataComponentCard.tsx
+++ src/ui_components/DataComponentCard.tsx
@@ -1,13 +1,23 @@
+import { useLocation } from "preact-iso"
+
 import { DataComponent } from "core/data/interface"
 
+import { ROUTES } from "../routes"
 import { sanitize_with_TipTap } from "../text_editor/sanitise_html"
 import "./DataComponentCard.css"
 
 
 export function DataComponentCard({ data_component }: { data_component: DataComponent })
 {
+    const location = useLocation()
+
     return (
-        <div className="data-component-card">
+        <div
+            className="data-component-card"
+            onClick={() => {
+                location.route(ROUTES.DATA_COMPONENT.VIEW(data_component.id))
+            }}
+        >
             <h3 dangerouslySetInnerHTML={{ __html: sanitize_with_TipTap(data_component.title, true) }} />
             <p dangerouslySetInnerHTML={{ __html: sanitize_with_TipTap(data_component.description, false) }} />
         </div>
