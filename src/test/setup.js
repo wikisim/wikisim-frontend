5dba272dbe2f1ed681bff92cb18d4c4560577fb0
Alexander James Phillips <ajp.global@gmail.com>
2025-07-24 18:55:32 +0100

    Add JSDOM for running tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,5 +1,4 @@
-// Allows for failing test suite when a test throws an async error
-
+// Ensures test suite will fail when a test throws an async error
 
 process.on("unhandledRejection", (err) =>
 {
@@ -10,3 +9,17 @@ process.on("uncaughtException", (err) =>
 {
     throw err
 })
+
+
+// Sets up a DOM environment for testing
+import { JSDOM } from "jsdom"
+
+const dom = new JSDOM("<!doctype html><html><body></body></html>", { url: "http://localhost" })
+global.window = dom.window
+global.document = dom.window.document
+global.DOMParser = dom.window.DOMParser
+global.Node = dom.window.Node
+global.navigator = dom.window.navigator
+global.HTMLElement = dom.window.HTMLElement
+global.Element = dom.window.Element
+global.getComputedStyle = dom.window.getComputedStyle
