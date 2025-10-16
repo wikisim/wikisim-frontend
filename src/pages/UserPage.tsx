a2ee264c3f8ed0c1408a448a5a08acdb9575ab2e
Alexander James Phillips <ajp.global@gmail.com>
2025-10-16 17:57:20 +0100

    Add toggle to user profile to allow seeing option to edit using code editor

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@mantine/core"
-import { useEffect } from "preact/hooks"
+import { Button, Checkbox } from "@mantine/core"
+import IconHelpCircle from "@tabler/icons-react/dist/esm/icons/IconHelpCircle"
+import { useEffect, useState } from "preact/hooks"
 
+import { TargetedEvent } from "preact/compat"
+import { HelpToolTip } from "../buttons/HelpText"
 import { ROUTES } from "../routes"
+import { local_storage } from "../state/local_storage"
 import { app_store } from "../state/store"
 import { get_async_user } from "../state/users/accessor"
 import { NewDataComponentButtons } from "../ui_components/NewDataComponentButtons"
@@ -14,6 +18,8 @@ export function UserPage(props: { user_id_or_name: string })
     const state = app_store()
     const async_user = get_async_user(state, props.user_id_or_name)
     const { user, status } = async_user
+
+    const [show_option_for_code_editor, set_show_option_for_code_editor] = useState(local_storage.get_show_option_for_code_editor())
 
     if (!user)
     {
@@ -42,8 +48,7 @@ export function UserPage(props: { user_id_or_name: string })
     return <>
         <div className="page-container">
             <h2>
-                <span style={{ fontSize: 15, fontWeight: "normal" }}>User page for</span>
-                {" " + user.name}
+                {user.name}'s profile
             </h2>
 
             <div className="user-page-row">
@@ -51,7 +56,7 @@ export function UserPage(props: { user_id_or_name: string })
                     component="a"
                     href={ROUTES.DATA_COMPONENT.SEARCH({ user_id: user.id })}
                     variant="primary-user"
-                    size="md"
+                    size="sm"
                 >{user.name}'s pages</Button>
             </div>
 
@@ -63,13 +68,33 @@ export function UserPage(props: { user_id_or_name: string })
                     component="a"
                     href={ROUTES.DATA_COMPONENT.SEARCH({ user_id: user.id })}
                     variant="primary-user"
-                    size="md"
+                    size="sm"
                 >Edits by {user.name} (not implemented yet)</Button>
             </div>
 
             <div class="vertical-gap" />
 
-            {is_you && <NewDataComponentButtons button_size="md" />}
+            {is_you && <NewDataComponentButtons button_size="sm" />}
+
+            <div class="vertical-gap" />
+
+            {is_you && <HelpToolTip
+                message={`When enabled, this will show a toggle next to the Value Input text field to allow you to swap between the normal text editor and the prototype code editor.  This is still a work in progress so it may partially corrupt your code when you switch back and forth.`}
+            >
+                <div style={{ display: "flex", width: "fit-content" }}>
+                    <Checkbox
+                        size="md"
+                        label="Allow use of prototype code editor"
+                        onChange={(e: TargetedEvent<HTMLInputElement, Event>) =>
+                        {
+                            set_show_option_for_code_editor(e.currentTarget.checked)
+                            local_storage.set_show_option_for_code_editor(e.currentTarget.checked)
+                        }}
+                        checked={show_option_for_code_editor}
+                    />
+                    <IconHelpCircle size={16} style={{ marginLeft: 4 }} />
+                </div>
+            </HelpToolTip>}
         </div>
     </>
 }
