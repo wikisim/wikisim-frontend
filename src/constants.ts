48ae3b587ba5fa6fbd416d1d76af46309dd37df6
Alexander James Phillips <ajp.global@gmail.com>
2025-11-07 13:41:51 +0000

    Change colour order

--- src/constants.ts
+++ src/constants.ts
@@ -8,13 +8,13 @@ export const colour_mismatch_line = "rgb(255, 168, 168)" // --requires-manual-sy
 
 const colours = [
     to_rgba(colour_actual),
+    to_rgba(colour_mismatch),
     to_rgba(colour_expected),
     "rgba(47, 158, 68, 1)",    // success green (#2f9e44)
-    to_rgba(colour_mismatch),
-    "rgba(102, 51, 153, 1)",   // purple (#663399)
     "rgba(255, 102, 0, 1)",    // bright orange (#ff6600)
     "rgba(0, 123, 167, 1)",    // teal (#007ba7)
     "rgba(153, 0, 76, 1)",     // dark pink (#99004c)
+    "rgba(102, 51, 153, 1)",   // purple (#663399)
 ]
 export function get_line_graph_colour(index: number, opacity: number = 1): string
 {
