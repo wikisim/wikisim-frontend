831bee4e1c93ff28354b2e813b2637e739df5ce5
Alexander James Phillips <ajp.global@gmail.com>
2025-11-07 13:48:04 +0000

    Fix test

--- src/ui_components/data_wrangling/event_and_state_handlers.test.ts
+++ src/ui_components/data_wrangling/event_and_state_handlers.test.ts
@@ -1,41 +1,41 @@
 import { expect } from "chai"
 
-import { make_alias } from "./event_and_state_handlers"
+import { make_name_from_path } from "./event_and_state_handlers"
 
 
-describe("make_alias", function ()
+describe("make_name_from_path", function ()
 {
     it("creates alias from key", function ()
     {
-        const alias = make_alias([{ key: "name" }], [])
+        const alias = make_name_from_path([{ key: "name" }], {})
         expect(alias).equals("name")
     })
 
     it("creates alias from nested key", function ()
     {
-        const alias = make_alias([{ key: "user" }, { key: "name" }], [])
+        const alias = make_name_from_path([{ key: "user" }, { key: "name" }], {})
         expect(alias).equals("name")
     })
 
     it("creates alias from index", function ()
     {
-        const alias = make_alias([{ index: 0 }], [])
+        const alias = make_name_from_path([{ index: 0 }], {})
         expect(alias).equals("index_0")
     })
 
     it("prefers to create alias from key", function ()
     {
-        const alias = make_alias([{ key: "user" }, { index: 0 }], [])
+        const alias = make_name_from_path([{ key: "user" }, { index: 0 }], {})
         expect(alias).equals("user_0")
     })
 
     it("creates unique alias when there is a conflict", function ()
     {
-        const existing_paths = [
-            "name",
-            "user_name",
-        ].map(alias => ({ path: [], alias }))
-        const alias = make_alias([{ key: "second" }, { key: "user" }, { key: "name" }], existing_paths)
+        const existing_names = {
+            '[{"key":"name"}]': "name",
+            '[{"key":"user_name"}]': "user_name",
+        }
+        const alias = make_name_from_path([{ key: "second" }, { key: "user" }, { key: "name" }], existing_names)
         expect(alias).equals("second_user_name")
     })
 })
